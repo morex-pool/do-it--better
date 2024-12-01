4c21cfad13687a68a691f23a80be65b8e56032da
morteza <morteza@realworks.nl>
2024-12-01 10:01:12 +0100

    First version of basic implementation

---
+++ src/main/java/org/doitbetter/AnalyzeCodeQualityAction.java
@@ -0,0 +1,23 @@
+package org.doitbetter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+
+public class AnalyzeCodeQualityAction extends AnAction {
+	@Override
+	public void actionPerformed(AnActionEvent e) {
+		Project project = e.getProject();
+		Map<String, String> requirements = new HashMap<>();
+		requirements.put("Unused Private Method", "java");
+		if (project != null) {
+			(new CodeQualityDetector()).start(project, requirements);
+		} else {
+			System.err.println("No project found. Ensure you are running this action within a project context.");
+		}
+
+	}
+}
