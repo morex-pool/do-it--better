cd5f7eb461849dd66741de45ae932f5fe38c6c2e
morteza <morteza@realworks.nl>
2024-12-24 18:24:30 +0100

    Remove not required things

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.25"
     id("org.jetbrains.intellij") version "1.17.2"
 }
 
@@ -9,34 +8,18 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    google()
-    gradlePluginPortal()
-    mavenLocal()
-    maven {
-        url = uri("https://packages.jetbrains.team/maven/p/idea/dev")
-    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2024.1") // Use the appropriate IntelliJ IDEA version
-    type.set("IC") // IntelliJ IDEA Community Edition
-    plugins.set(listOf("java")) // Add additional plugins if needed
+    version.set("2024.1")
+    type.set("IC")
+    plugins.set(listOf("java"))
 }
 
 tasks {
-    // Set the JVM compatibility versions
     withType<JavaCompile> {
         sourceCompatibility = "17"
         targetCompatibility = "17"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
-
-    prepareSandbox {
-        dependsOn(processResources)
     }
 
     patchPluginXml {
@@ -44,17 +27,7 @@ tasks {
         untilBuild.set("242.*")
     }
 
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    prepareSandbox {
+        dependsOn(processResources)
     }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
-}
-
-dependencies {
-    // Additional dependencies can be added if needed
 }
